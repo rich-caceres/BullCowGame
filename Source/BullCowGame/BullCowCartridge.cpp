d1bbfc1257776e3fb402153d361d3c701d814d9e
rich-caceres <richard.caceres22590@outlook.com>
2021-01-18 19:00:20 -0500

    Add project files.

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the game!"));
+    PrintLine(TEXT("Guess the 10 letter word...")); //TODO: change the number 10 to a dynamically showing number
+    PrintLine(TEXT("Please press enter when complete..."));
+    HiddenWord = TEXT("abolishment"); //sets the HiddenWord
+    //TODO: Set Lives
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    
+    ClearScreen();
+    PrintLine(Input);//echos the player guess
+
+    //checks player guess
+    if (Input == HiddenWord) 
+    {
+
+        PrintLine(TEXT("You have won the game! Good for you"));
+
+    }
+    else
+    {
+        
+        PrintLine(TEXT("Incorrect word. Try again!"));
+        //TODO: need to remove life
+        
+        
+    }
+
+    //TODO: check if life > 0
+    //TODO: if life !> 0
+    //TODO: end of game, you have lost. Print HiddenWord
+    //TODO: do you want to play again?
+    //TODO: else loop to the beginning.
+}
